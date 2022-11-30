2e905f9d1d55c2b9833c73755bd2ef9790947c85
Арина Задорожняя <zadoroozhnyayaa@gmail.com>
2022-11-30 11:35:33 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pz_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\new_file.txt";
+            FileStream file = new FileStream(path, FileMode.Create);
+            StreamWriter writer = new StreamWriter(file);
+            writer.Write("А ну-ка, Бишка, прочти, что в книжке написано! Понюхала собачка книжку, да и прочь пошла. Не моё, - говорит, - дело книги читать; я дом стерегу, по ночам не сплю, лаю, воров да волков пугаю, на охоту хожу, зайку слежу, уточек ищу, поноску тащу - будет с меня и этого.");
+            writer.Close();
+
+
+            StreamReader reader = new StreamReader(path);
+            string array = reader.ReadToEnd();
+            if (array != "")
+            {
+                Console.WriteLine(array);
+            }
+            else
+            {
+                Console.WriteLine("File is empty");
+            }
+        }
+    }
+}
